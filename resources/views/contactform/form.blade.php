f82dfb4f6f4befc6aafdc073be48bbdfcd5908f9
Bastiyan <bastiyan.rodrigo@considerthisuk.com>
2019-05-10 11:13:29 +0100

    Made a slight change in the route.php so it loads the contact form first

--- resources/views/contactform/form.blade.php
+++ resources/views/contactform/form.blade.php
@@ -1,9 +1,13 @@
-<html>
-		<head>
-			<meta name="csrf-token" content="{{ csrf_token() }}">
-			<link href="{{ asset('/css/app.css') }}" rel="stylesheet"> 
-		<title>Contact Form</title>
-		</head>
+<!DOCTYPE html>
+<html lang='en'>
+	<head>
+		<meta name="csrf-token" content="{{ csrf_token() }}">
+		 <meta charset="utf-8">
+ 		 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+ 		 <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="{{ asset('/css/app.css') }}" rel="stylesheet"> 
+		<title>Submission Form</title>
+	</head>
 
 	<body>
 
